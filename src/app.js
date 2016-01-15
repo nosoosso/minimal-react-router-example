90bf4ca7ebb24677ce1a7b690daee58431f9e761
nosoosso <nosoosso.null@gmail.com>
2016-01-15 23:43:18 +0900

    URLがハッシュになっていたのを修正

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom'
 
-import { Router, Route, Link, browserHistory } from 'react-router'
+import { Router, Route, Link } from 'react-router'
+import createBrowserHistory from 'history/lib/createBrowserHistory'
 
 import Index from './component/index.jsx'
 import Hoge from './component/hoge.jsx'
@@ -10,7 +11,7 @@ import Fuga from './component/fuga.jsx'
 window.App = {
     render: function () {
         ReactDom.render(
-            <Router history={browserHistory}>
+            <Router history={createBrowserHistory()}>
                 <Route path="/" component={Index}>
                     <Route path="hoge" component={Hoge}/>
                     <Route path="fuga" component={Fuga}/>
